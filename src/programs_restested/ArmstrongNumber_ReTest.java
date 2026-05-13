cb0f4e284483eaca79b799cb582a3b656dcad878
gaurav.arora <gauravarora.epri@gmail.com>
2026-05-14 02:51:07 +0530

    added revised code - 14 may 2026

--- src/programs_restested/ArmstrongNumber_ReTest.java
+++ src/programs_restested/ArmstrongNumber_ReTest.java
@@ -1,9 +1,28 @@
 package programs_restested;
 
+// Number=163 , Armstrong of number would be 1*1*1+6*6*6+3*3*3
+
 public class ArmstrongNumber_ReTest
 {
 
+    static void main() {
 
+            int number=163;
+            String number_in_string_format=String.valueOf(number);
+            int sum=0;
+            for(int i=0;i<number_in_string_format.length();i++)
+            {
+                        int multiply=1;
+                for(int j=0;j<number_in_string_format.length();j++)
+                {
+                    multiply=multiply*Integer.parseInt(number_in_string_format.charAt(i)+"");
+                }
+                sum=sum+multiply;
+
+            }
+        System.out.println(sum);
+
+    }
 
 }
 
